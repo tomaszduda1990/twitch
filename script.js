cf321d2291e9495452bda99fd451e0fb9f09a57e
Tomek <tomasz.duda.krk@gmail.com>
2017-09-30 17:12:30 +0200

    new design added

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ const clientID = {
  }
 const url = 'https://api.twitch.tv/kraken/';
 const imgUrl = 'imgs/Glitch_Purple_RGB.png';
-let userData = [];
 const list = document.querySelector('.list');
+const buttons = document.querySelectorAll('.btn');
+const onlineBtn = document.querySelector('.online');
+const allBtn = document.querySelector('.all');
+const offlineBtn = document.querySelector('.offline');
+console.log(buttons);
+let filtered = [];
+
 
 function apiCall(arr, type){
 	type = 'streams/';
@@ -17,11 +23,11 @@ function apiCall(arr, type){
 	 		url: url+type+name,
 	 		headers: clientID,
 	 		success: function(data) {
+	 			console.log(data);
 			   status = isOnline(data)[0];
 			   game = isOnline(data)[1];
-			   console.log(status);
 			   if(status == "online"){
-			   		list.innerHTML += populateList(data.stream.channel, game, status);
+			   		list.innerHTML += populateList(data.stream.channel, game, status, data.stream.viewers);
 			   }else{
 			   	$.ajax({
 					type: 'GET',
@@ -32,6 +38,8 @@ function apiCall(arr, type){
 					}
 				})
 			   }
+			   status = "";
+			   game = "";
 			 },
 		 	error: function(){
 		 		console.log('error!')
@@ -50,12 +58,11 @@ function isOnline(data){
       };
       return [status, game];
 }
-function populateList(data, game, status){
-		console.log(status);
+function populateList(data, game, status, viewers){
 	if(status == "online"){
-		return `<li class= "online">
-					<img class="portrait" src = "${data.logo ? data.logo : imgUrl}" alt = "logo of ${data.display_name || data.name}">
-					<a href = "${data.url}">
+		return `<li class= "online" >
+					<a href = "${data.url}" target = "_blank">
+						<img class="portrait" src = "${data.logo ? data.logo : imgUrl}" alt = "logo of ${data.display_name || data.name}">
 						<div class = 'item'>
 							<h5>${data.display_name || data.name}</h5>
 							<p>${game}</p>
@@ -63,16 +70,15 @@ function populateList(data, game, status){
 					</a>
 					<div class ="activness">
 						<img src ="imgs/active.png" alt = "active">
-						<p>online</p>
+						<p>${viewers} viewers</p>
 					</div>
 				</li>`;
 	}else{
 		return `<li class ="offline">
-					<img class="portrait" src = "${data.logo ? data.logo : imgUrl}" alt = "logo of ${data.display_name}">
-					<a href = "${data.url}">
+					<a href = "${data.url}" target = "_blank">
+						<img class="portrait" src = "${data.logo ? data.logo : imgUrl}" alt = "logo of ${data.display_name}">
 						<div class = 'item'>
 							<h5>${data.display_name}</h5>
-							<p>Offline</p>
 						</div>
 					</a>
 					<div class ="activness">
@@ -82,4 +88,41 @@ function populateList(data, game, status){
 				</li>`;	
 	}
 }
+function switchClass(item){
+	buttons.forEach(button =>{
+		button.removeAttribute('id');
+	});
+	item.setAttribute('id', 'active');
+}
+
+function sortList(){
+	switchClass(this);
+	const classCheck = this.classList.value;
+	const nodeList = list.querySelectorAll('li');
+	if(classCheck.includes('all')){
+		list.innerHTML = "";
+		apiCall(usernames);
+	}else if(classCheck.includes('on')){
+		nodeList.forEach(node => {
+			if(node.classList.contains('offline')){
+				node.classList.add('sortedOut');
+			}else{
+				node.classList.remove('sortedOut');
+			}
+		});
+	}else if(classCheck.includes('off')){
+		nodeList.forEach(node => {
+			if(node.classList.contains('online')){
+				node.classList.add('sortedOut');
+			}else{
+				node.classList.remove('sortedOut');
+			}
+		});
+	}
+	
+	
+}
+buttons.forEach(button => button.addEventListener('click', sortList));
+
+
 apiCall(usernames);
